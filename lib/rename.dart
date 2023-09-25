69f809eab081c525ffa4271d8929f1ea4c63b5ff
RicardiMoneyP <130101756+RicardiMoneyP@users.noreply.github.com>
2023-09-24 21:26:50 -0300

    Update rename.dart

--- lib/rename.dart
+++ lib/rename.dart
@@ -17,36 +17,36 @@ Future changeAppName(String? appName, Iterable<Platform> platforms) async {
   if (platforms.isEmpty || platforms.contains(Platform.ios)) {
     await fileRepository.changeIosAppName(appName);
   }
-  if (platforms.isEmpty || platforms.contains(Platform.macOS)) {
-    await fileRepository.changeMacOsAppName(appName);
-  }
+  // if (platforms.isEmpty || platforms.contains(Platform.macOS)) {
+  //   await fileRepository.changeMacOsAppName(appName);
+  // }
   if (platforms.isEmpty || platforms.contains(Platform.android)) {
     await fileRepository.changeAndroidAppName(appName);
   }
-  if (platforms.isEmpty || platforms.contains(Platform.linux)) {
-    await fileRepository.changeLinuxAppName(appName);
-  }
-  if (platforms.isEmpty || platforms.contains(Platform.web)) {
-    await fileRepository.changeWebAppName(appName);
-  }
-  if (platforms.isEmpty || platforms.contains(Platform.windows)) {
-    await fileRepository.changeWindowsAppName(appName);
-  }
+  // if (platforms.isEmpty || platforms.contains(Platform.linux)) {
+  //   await fileRepository.changeLinuxAppName(appName);
+  // }
+  // if (platforms.isEmpty || platforms.contains(Platform.web)) {
+  //   await fileRepository.changeWebAppName(appName);
+  // }
+  // if (platforms.isEmpty || platforms.contains(Platform.windows)) {
+  //   await fileRepository.changeWindowsAppName(appName);
+  // }
 }
 
 Future changeBundleId(String? bundleId, Iterable<Platform> platforms) async {
   if (platforms.isEmpty || platforms.contains(Platform.ios)) {
     await fileRepository.changeIosBundleId(bundleId: bundleId);
   }
-  if (platforms.isEmpty || platforms.contains(Platform.macOS)) {
-    await fileRepository.changeMacOsBundleId(bundleId: bundleId);
-  }
+  // if (platforms.isEmpty || platforms.contains(Platform.macOS)) {
+  //   await fileRepository.changeMacOsBundleId(bundleId: bundleId);
+  // }
   if (platforms.isEmpty || platforms.contains(Platform.android)) {
     await fileRepository.changeAndroidBundleId(bundleId: bundleId);
   }
-  if (platforms.isEmpty || platforms.contains(Platform.linux)) {
-    await fileRepository.changeLinuxBundleId(bundleId: bundleId);
-  }
+  // if (platforms.isEmpty || platforms.contains(Platform.linux)) {
+  //   await fileRepository.changeLinuxBundleId(bundleId: bundleId);
+  // }
 }
 
 Future<String?> getIosAppName() async {
